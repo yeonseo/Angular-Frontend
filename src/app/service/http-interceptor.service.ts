1f8528701d5fd4e22ea4de42d32d6ed912b6bf5b
yeonseo <yulga_nara@naver.com>
2020-06-03 16:19:45 +0900

    Interceptor 에서 localStorage에 저장된 토큰값을 가지고 Header에 셋팅하도록 수정함

--- src/app/service/http-interceptor.service.ts
+++ src/app/service/http-interceptor.service.ts
@@ -1,16 +1,41 @@
-import { Injectable } from '@angular/core';
+import {Injectable, Injector} from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs/';
 import { catchError } from 'rxjs/operators';
+import {RequestService} from "./request.service";
+import {AuthenticationService} from "./authentication.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpInterceptorService implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const request: HttpRequest<any> = req.clone();
 
-    console.log('interceptor');
+  private globalToken = 'EmptyToken';
+
+  constructor(
+    private injector: Injector,
+    private requestService: RequestService,
+    private authService: AuthenticationService
+  ) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.authService.currentUser) {
+      this.authService.currentUser.subscribe((data: any) =>
+        this.globalToken = data.token
+      );
+    }
+    const request = req.headers.has('Authorization') ?
+      req : req.clone({setHeaders: { 'Authorization': this.globalToken }});
+
+    // if (!request.headers.has('Authorization')) {
+    //   req = req.clone({
+    //     headers: req.headers.set('Authorization', this.globalToken)
+    //   });
+    // }
+
+    const tokenValue = request.headers.get('Authorization');
+    console.log('interceptor : ' + tokenValue);
     return next.handle(request).pipe(
       catchError(e => {
         console.log(e.error.error.message);
